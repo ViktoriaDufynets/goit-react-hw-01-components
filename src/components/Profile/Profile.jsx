bd6b0b383bbcb1395e814d1e77cc5bb9ccf3e3aa
ViktoriaDufynets <52819403+ViktoriaDufynets@users.noreply.github.com>
2023-05-22 11:19:57 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import css from './profile.module.css';
+import css from './Profile.module.css';
 
 export const Profile = ({username, tag, location, avatar,   stats: { followers, views, likes }}) => {
     return (
